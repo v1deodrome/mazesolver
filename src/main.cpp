5d269868eb1d90b80f5e3a639daf1801923b0ed9
jeverill0462954 <w0462954@campus.nscc.ca>
2024-02-22 21:56:49 -0400

    Assignment complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <fstream>
 #include <iostream>
 #include "maze_solver.h"
-#include <filesystem>
 
 using namespace std;
 
@@ -12,12 +11,16 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     else {
+        cout << "Running Maze Solver..." << endl;
         MazeSolver solver(argv[1]);
+        cout << "Solving..." << endl;
         string solution = solver.solve_maze();
+        cout << "Done!" << endl;
         ofstream output;
         output.open(argv[2]);
         output << solution;
         output.close();
+        cout << "Saved to " << argv[2] << "." << endl;
         return 0;
     }
 }
